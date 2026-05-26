e865831f88358d37032a12338e70c64bbf105e2f
byronic-games <byronicman@gmail.com>
2026-05-26 20:23:53 +0100

    Grid display tweaks

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_VERSION = "20260526c";
+const CACHE_VERSION = "20260526d";
 const CACHE_NAME = `byronic-52-${CACHE_VERSION}`;
-const GAME_ASSET_VERSION = "20260526c";
+const GAME_ASSET_VERSION = "20260526d";
 
 const APP_SHELL = [
   "./",
